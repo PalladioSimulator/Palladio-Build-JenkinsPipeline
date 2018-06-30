6e89d4fa746dbe49dd5509c11b73a143ecc711bb
Stephan Seifermann <seifermann@fzi.de>
2018-06-30 10:34:25 +0200

    Added some debugging output.

--- vars/palladioBuild.groovy
+++ vars/palladioBuild.groovy
@@ -45,10 +45,10 @@ def call(body) {
 					"PATH+MAVEN=${tool MAVEN_TOOL_NAME}/bin:${env.JAVA_HOME}/bin"
 				]) {
 					println JAVA_TOOL_NAME
-					println ${env.JAVA_HOME}
+					println env.JAVA_HOME
 					println MAVEN_TOOL_NAME
-					println ${env.MAVEN}
-					println ${env.PATH}
+					println env.MAVEN
+					println env.PATH
 					genericSh "mvn clean verify"
 				}
 			}
