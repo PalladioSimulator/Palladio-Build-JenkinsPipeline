c7e32541d95ed6cd08bd0cfc758b7e23c3c03cf6
Stephan Seifermann <seifermann@fzi.de>
2018-07-13 19:15:56 +0200

    Included mail notification.

--- vars/palladioBuild.groovy
+++ vars/palladioBuild.groovy
@@ -21,6 +21,7 @@ def call(body) {
 		}
 
 		boolean skipCodeQuality = config.containsKey('skipCodeQuality') && config.get('skipCodeQuality').toString().trim().toBoolean()
+		boolean skipNotification = config.containsKey('skipNotification') && config.get('skipNotification').toString().trim().toBoolean()
 
 		boolean doReleaseBuild = params.DO_RELEASE_BUILD.toString().toBoolean()
 		String releaseVersion = params.RELEASE_VERSION		
@@ -127,8 +128,25 @@ def call(body) {
 				}
 			}
 		} catch (err) {
-			currentBuild.result = 'FAILED'
+			currentBuild.result = 'FAILURE'
+			if (err instanceof hudson.AbortException && err.getMessage().contains('script returned exit code 143')) {
+				currentBuild.result = 'ABORTED'
+			}
+			if (err instanceof org.jenkinsci.plugins.workflow.steps.FlowInterruptedException && err.causes.size() == 0) {
+				currentBuild.result = 'ABORTED'
+			}
 			throw err
+		} finally {
+			def currentResult = currentBuild.result ?: 'SUCCESS'
+			def previousResult = currentBuild.previousBuild?.result ?: 'SUCCESS'
+
+			if (!skipNotification) {
+				if (currentResult == 'FAILURE') {
+					notifyFailure()
+				} else if (currentResult == 'SUCCESS' && previouslyFailed()) {
+					notifyFix()
+				}
+			}
 		}
 	}
 }
@@ -139,5 +157,35 @@ def genericSh(cmd) {
 	}
 	else {
 		bat cmd
+	}
+}
+
+def notifyFix() {
+	notify('FIXED', 'fixed previous build errors')
+}
+
+def notifyFailure() {
+	notify('FAILED', 'failed')
+}
+
+def notify(token, verb) {
+	mail([
+		subject: "${token}: build of ${JOB_NAME} #${BUILD_NUMBER}",
+		body: "The build of ${JOB_NAME} #${BUILD_NUMBER} ${verb}.\nPlease visit ${BUILD_URL} for details.",
+		to: new String('cGFsbGFkaW8tYnVpbGRAaXJhLnVuaS1rYXJsc3J1aGUuZGU='.decodeBase64())
+	])
+}
+
+def previouslyFailed() {
+	for (buildObject = currentBuild.previousBuild; buildObject != null; buildObject = buildObject.previousBuild) {
+		if (buildObject.result == null) {
+			continue
+		}
+		if (buildObject.result == 'FAILURE') {
+			return true
+		}
+		if (buildObject.resultIsBetterOrEqualTo('UNSTABLE')) {
+			return false
+		}
 	}
 }
