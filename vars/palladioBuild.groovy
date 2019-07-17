d9344c7522e50b9b0ce981790dd2ac630b17fc51
Stephan Seifermann <stephan.seifermann@kit.edu>
2019-07-17 15:29:52 +0200

    Use maven 3.6.0

--- vars/palladioBuild.groovy
+++ vars/palladioBuild.groovy
@@ -1,8 +1,10 @@
+@Library('MDSD.tools') _
+
 def call(body) {
 	
 	final MAIL_DEFAULT_RECIPIENT = new String('cGFsbGFkaW8tYnVpbGRAaXJhLnVuaS1rYXJsc3J1aGUuZGU='.decodeBase64())
-	final BUILD_IMAGE = 'maven:3-jdk-11'
-	final BUILD_LIMIT_TIME = 30
+	final BUILD_IMAGE = 'maven:3.6.0-jdk-11'
+	final BUILD_LIMIT_TIME = 40
 	final BUILD_LIMIT_RAM = '4G'
 	final BUILD_LIMIT_HDD = '20G'
 	final SSH_NAME = 'SDQ Webserver Eclipse Update Sites'
@@ -10,4 +12,5 @@ def call(body) {
 	
 	library('MDSD.tools')
 	slaveEclipsePipeline(body, SSH_NAME, WEB_ROOT, MAIL_DEFAULT_RECIPIENT, BUILD_IMAGE, BUILD_LIMIT_TIME, BUILD_LIMIT_RAM, BUILD_LIMIT_HDD)
+
 }
