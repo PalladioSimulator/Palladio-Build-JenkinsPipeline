b00ee0ca06176dabbebc7f36d14743e1c299efb8
Stephan Seifermann <9149072+seiferma@users.noreply.github.com>
2019-09-14 10:02:50 +0200

    Reuse MDSD.tools pipeline (#2)
    * Reuse MDSD.tools build library.
    * Update palladioBuild.groovy
    * Use maven 3.6.0
    * Fixed build issue
    * Added new PalladioPipeline
    * Added required settings id and goal for maven step

---
+++ vars/PalladioPipeline.groovy
@@ -0,0 +1,32 @@
+def call(body) {
+    AbstractMDSDToolsDSLPipeline {
+        agent_label = 'docker'
+
+        buildWithMaven {
+            version = '3.6.0'
+            jdkVersion = 11
+            settingsId = 'fba2768e-c997-4043-b10b-b5ca461aff54'
+            goal = 'clean verify'
+        }
+
+        constraintBuild {
+            timeLimitMinutes = 30
+            ramLimit = '4G'
+            hddLimit = '20G'
+        }
+        
+        skipDeploy ("${this.BRANCH_NAME}" != 'master')
+        skipNotification ("${this.BRANCH_NAME}" != 'master')
+        
+        deployUpdatesiteSshName 'web'
+        deployUpdatesiteRootDir '/home/sftp/data'
+        deployUpdatesiteSubDir ("${this.BRANCH_NAME}" == 'master' ? 'nightly': "branches/${this.BRANCH_NAME}")
+        deployUpdatesiteProjectDir this.scm.userRemoteConfigs[0].url.replaceFirst(/^.*\/([^\/]+?).git$/, '$1').toLowerCase()
+             
+        notifyDefault 'cGFsbGFkaW8tYnVpbGRAaXJhLnVuaS1rYXJsc3J1aGUuZGU='
+
+        body.delegate = delegate
+        body.resolveStrategy = Closure.DELEGATE_FIRST
+        body()
+    }
+}
